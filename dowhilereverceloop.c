ed41b67b586f1cbef8b1813939e45bfa14eea51e
Jahanzab-Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-08-19 02:47:49 -0700

    Add files via upload

---
+++ dowhilereverceloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main () {
+    int n;
+    printf("enter the value :");
+    scanf("%d" , &n);
+
+
+    int i = n;
+    do{
+        printf("%d \n" , i);
+        i--;
+    } while(i>=0);
+
+
+return 0;
+}
